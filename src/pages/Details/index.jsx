19ac167dcc7665e61b9097b9f19b41475b42c800
rafaverde <rafaverde@msn.com>
2024-03-14 15:30:07 -0300

    :tada: Initial Commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./style"
+import { Button } from "../../components/Button"
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello, world!</h1>
+      <p>Starting React studies.</p>
+
+      <Button title="Voltar" loading />
+    </Container>
+  )
+}
---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  body, button, input, textarea {
+    font-family: ${({ theme }) => theme.FONTS.PRIMARY_FONT};
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+  
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+`
