95c532ced47747f106a8c38ef5c4d481969b84dd
rafaverde <rafaverde@msn.com>
2024-03-15 15:45:15 -0300

    ButtonText element implemented and Details Page Stylization finished

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,32 +1,48 @@
-import { Container, Links } from "./style"
+import { Container, Links, Content } from "./style"
 
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir a nota" />
 
-      <Section title="Marcadores">
-        <Tag title="explorer"></Tag>
-        <Tag title="nodejs"></Tag>
-      </Section>
+          <h1>Introdução ao React.js</h1>
 
-      <Button title="Voltar" loading />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi,
+            nam laborum. Et veritatis repudiandae voluptatibus perferendis,
+            distinctio aliquid impedit necessitatibus officia quibusdam eligendi
+            expedita alias molestias. Ad beatae aliquid odit.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="explorer"></Tag>
+            <Tag title="nodejs"></Tag>
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
