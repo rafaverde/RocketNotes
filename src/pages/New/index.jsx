772a5c99bcab3327f8a8052df807822cd3830f3d
rafaverde <rafaverde@msn.com>
2024-04-24 18:19:50 -0300

    Link add in note function

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import { Container, Form } from "./styles"
 import { FiChevronLeft } from "react-icons/fi"
 import { Link } from "react-router-dom"
@@ -10,6 +12,31 @@ import { NoteItem } from "../../components/NoteItem"
 import { Button } from "../../components/Button"
 
 export function New() {
+  // Cria estado para armazenar os links
+  const [links, setLinks] = useState([])
+  // Cria estado para armazenar o link adicionado
+  const [newLink, setNewLink] = useState("")
+
+  // Lida com click ao adicionar novo link
+  function handleAddLink() {
+    // Verifica se está vazio
+    if (!newLink) {
+      alert("Não pode ser vazio, digite um link")
+    } else {
+      // Define o estado atual, pegando o que já esxiste nele e monta um novo vetor
+      setLinks((prevState) => [...prevState, newLink])
+      // Reseta o estado do link para um novo ser usado
+      setNewLink("")
+    }
+  }
+
+  // Lida com o clique de deletar link
+  function handleRemoveLink(deleted) {
+    // Coloca no estado, uma lista filtrando o que for passado em deleted
+    // gerando uma lista apenas com o que for diferente dele
+    setLinks((prevState) => prevState.filter((link) => link !== deleted))
+  }
+
   return (
     <Container>
       <Header />
@@ -28,8 +55,23 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="https://www.rocketseat.com.br"></NoteItem>
-            <NoteItem placeholder="https://www.novolink.com" isNew></NoteItem>
+            {links.map((link, index) => (
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => {
+                  handleRemoveLink(link)
+                }}
+              ></NoteItem>
+            ))}
+
+            <NoteItem
+              placeholder="https://www.novolink.com"
+              isNew
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            ></NoteItem>
           </Section>
 
           <Section title="Marcadores">
