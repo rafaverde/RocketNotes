a7ce1bab99ff727e74b17dc0843d72fe1e675586
rafaverde <rafaverde@msn.com>
2024-04-30 15:56:36 -0300

    Tag Menu selection implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 import { FaCirclePlus, FaMagnifyingGlass } from "react-icons/fa6"
 import { Container, Brand, Menu, Search, Content, NewNotes } from "./styles"
 
@@ -6,8 +8,44 @@ import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
 import { Input } from "../../components/Input"
 import { Note } from "../../components/Note"
+import { api } from "../../services/api"
 
 export function Home() {
+  // Estado para puxar as tags
+  const [tags, setTags] = useState([])
+  // Estado para guardar as tags selecionadas
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  // Lida com tag quando selecionada recebendo o nome da tag como parâmetro
+  function handleTagSelected(tagName) {
+    // Verifica se a tag clicada já existe entre as selecionadas
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    // Caso retorne verdadeiro, retira a tag da lista de selecionadas
+    if (alreadySelected) {
+      // Filtra as tags que retorna uma nova lista sem a tag clicada.
+      const filteredTags = tagsSelected.filter((tag) => tag !== tagName)
+      // Atualiza estado com as tags selecionadas
+      setTagsSelected(filteredTags)
+    } else {
+      // Atualizao estado, incluindo o que continha e o que foi clicado
+      setTagsSelected((prevState) => [...prevState, tagName])
+    }
+  }
+
+  // Qunado carregar o componente Home, executa o código
+  useEffect(() => {
+    // Função para buscar as tags em nossa api.
+    async function fetchTags() {
+      // Busca as tags na api e aloca em response
+      const response = await api.get("/tags")
+      // Atualiza o estado das Tags com o conteúdo de tags do banco de dados
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return (
     <Container>
       <Brand>
@@ -18,17 +56,24 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" $isactive></ButtonText>
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            $isactive={tagsSelected.length === 0}
+          ></ButtonText>
         </li>
-        <li>
-          <ButtonText title="Frontend"></ButtonText>
-        </li>
-        <li>
-          <ButtonText title="React"></ButtonText>
-        </li>
-        <li>
-          <ButtonText title="Node"></ButtonText>
-        </li>
+        {tags &&
+          tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                // Inclui o valor do nome da tag no estado setSelectedTag passando o nome da tag
+                onClick={() => handleTagSelected(tag.name)}
+                // Verifica se a tag está no estado e se sim, ela é selecionada
+                $isactive={tagsSelected.includes(tag.name)}
+              ></ButtonText>
+            </li>
+          ))}
       </Menu>
 
       <Search>
